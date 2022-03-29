85297fbd87a80da7168810a6bc51bad0cc854512
GiovanniCalia <johnca@outlook.it>
2022-03-29 11:10:20 +0200

    Creazione repo e i vari contenitori

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,23 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <header-boolflix />
+    <main-boolflix />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import HeaderBoolflix from './components/HeaderBoolflix.vue'
+import MainBoolflix from './components/MainBoolflix.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    HeaderBoolflix,
+    MainBoolflix
   }
 }
 </script>
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
